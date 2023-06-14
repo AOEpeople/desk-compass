cfcf349cf1818842963ba8e272001179e438058e
Theresa Henze <theresa.henze@aoe.com>
2023-06-14 15:29:18 +0200

    build: add semantic release step

---
+++ release.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  branches: [
+      "main"
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "CHANGELOG.md",
+        "changelogTitle": "# Desk Compass Changelog"
+      }
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        "npmPublish": false
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["CHANGELOG.md", "package.json"]
+      }
+    ]
+  ]
+}
