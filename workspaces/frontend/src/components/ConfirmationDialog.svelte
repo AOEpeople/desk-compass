c37dc752d3430dca181258423c9f20f143b94b39
resah <3407199+resah@users.noreply.github.com>
2023-10-30 21:42:13 +0100

    chore(frontend): remove svelte-headlessui (not Svelte4-ready)

--- workspaces/frontend/src/components/ConfirmationDialog.svelte
+++ workspaces/frontend/src/components/ConfirmationDialog.svelte
@@ -1,50 +1,82 @@
 <script lang="ts">
   import { _ } from 'svelte-i18n';
   import { createEventDispatcher } from 'svelte';
-  import { Dialog, DialogDescription, DialogOverlay, DialogTitle } from '@rgossiaux/svelte-headlessui';
 
+  let dialog: HTMLDialogElement;
   const dispatch = createEventDispatcher();
-
-  const close = () => dispatch('dialogclosed');
-  const confirm = () => dispatch('dialogconfirmed');
 
   export let isOpen = true;
   export let confirmationName = $_('page.confirm.confirm');
+
+  $: if (dialog && isOpen) {
+    dialog.showModal();
+  }
+
+  const close = () => {
+    dialog.close();
+    dispatch('dialogclosed');
+  };
+  const confirm = () => {
+    dialog.close();
+    dispatch('dialogconfirmed');
+  };
 </script>
 
-<Dialog
-  open={isOpen}
-  on:close={close}
-  class={'absolute top-0 w-full h-screen'}>
-  <DialogOverlay class={'z-[1355] absolute bg-black/50 overflow-y-auto w-full h-screen'} />
-  <div class="w-full h-screen flex flex-col justify-center items-center">
-    <div class="z-[2055] relative mx-auto p-10 w-1/2 shadow-lg rounded-md bg-white">
-      <div class="mx-auto flex flex-col divide-y divide-grey">
-        <div class="mb-4">
-          <DialogTitle class={'text-xl font-bold mb-2'}>
-            <slot name="title">{$_('page.confirm.title')}</slot>
-          </DialogTitle>
-          <DialogDescription
-            class={''}
-            as="div">
-            <slot name="description" />
-          </DialogDescription>
-        </div>
-        <div class="btn-group flex flex-shrink-0 flex-wrap items-center justify-end pt-4">
-          <button
-            type="button"
-            class="btn-secondary"
-            on:click={close}>
-            {$_('page.confirm.cancel')}
-          </button>
-          <button
-            type="button"
-            class="btn-primary"
-            on:click={confirm}>
-            {confirmationName}
-          </button>
-        </div>
+<dialog
+  bind:this={dialog}
+  on:close={close}>
+  <div class="p-10 mx-auto flex flex-col divide-y divide-grey">
+    <div class="mb-4">
+      <div
+        class="text-xl font-bold mb-2"
+        data-testid="confirmation-dialog-title">
+        <slot name="title">{$_('page.confirm.title')}</slot>
+      </div>
+      <div data-testid="confirmation-dialog-description">
+        <slot name="description" />
       </div>
     </div>
+    <div class="btn-group flex flex-shrink-0 flex-wrap items-center justify-end pt-4">
+      <button
+        type="button"
+        class="btn-secondary"
+        on:click={close}>
+        {$_('page.confirm.cancel')}
+      </button>
+      <button
+        type="button"
+        class="btn-primary"
+        on:click={confirm}>
+        {confirmationName}
+      </button>
+    </div>
   </div>
-</Dialog>
+</dialog>
+
+<style lang="postcss">
+  dialog::backdrop {
+    @apply z-[1355] absolute bg-black/50 overflow-y-auto w-full h-screen;
+  }
+  dialog[open] {
+    animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+  @keyframes zoom {
+    from {
+      transform: scale(0.95);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+  dialog[open]::backdrop {
+    animation: fade 0.2s ease-out;
+  }
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+</style>
